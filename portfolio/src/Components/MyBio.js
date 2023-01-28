e2a5814a46d5ab7a6485fa6a9ef23b2a8dcb6cb3
Kalpayatri <khusbu.mallick@gmail.com>
2023-01-28 09:23:07 +0530

    added new files

---
+++ portfolio/src/Components/MyBio.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './MyBio.css'
+
+const MyBio=(props)=>{
+  return(
+    <div>
+    </div>
+  )
+}
+
+export default MyBio
---
+++ portfolio/src/Components/MyProjects.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const MyProjects=(props)=>{
+  return(
+    <div>
+    </div>
+  )
+}
+
+export default MyProjects
